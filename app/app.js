bde3982a18175a58649f178569728d2d268483f2
Pascal MUANG-KHOT <mail@mkpascal.net>
2013-05-25 21:50:10 -0700

    Initial Commit

---
+++ app/app.js
@@ -0,0 +1,25 @@
+var webSite = angular.module('webSite', []);
+
+    webSite.config(function ($routeProvider) {
+        $routeProvider
+            .when('/view1',
+                {
+                    controller: 'ControllerOne',
+                    templateUrl: 'app/partials/view1.html'
+                })
+            .when('/view2',
+                {
+                    controller: 'ControllerOne',
+                    templateUrl: 'app/partials/view2.html'
+                })
+            .otherwise({ redirectTo: '/view1' });
+    });
+    
+
+    webSite.controller('ControllerOne', function ($scope){
+        $scope.cv = [
+            { header: 'In short', content: 'Im a web developer' },
+            { header: 'Experience', content: 'Check my experience' },
+        ];    
+        
+    });
